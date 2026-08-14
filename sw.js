40d16d5d13cc5cd78c65fe032c92ba5e5dca429b
cethits200 <cet020181@gmail.com>
2026-08-14 14:22:41 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,6 @@
+const CACHE='hit30-definitivo-v14';
+const ASSETS=['./','./index.html','./manifest-v13.webmanifest','./hit30-final-192.png','./hit30-final-512.png',
+'./catalogo-clasico.js','./catalogo-cine.js','./catalogo-latino.js','./catalogo-ot.js','./catalogo-verano.js','./catalogo-tv.js','./catalogo-espana.js'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
